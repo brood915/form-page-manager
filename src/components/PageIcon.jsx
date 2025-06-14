f98bd0682f566437479e19aa6ddc8fe2d45230e6
Kevin Yoon <brood915@gmail.com>
2025-06-14 17:39:28 -0400

    added pageicon

---
+++ src/components/PageIcon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Info, FileText, CheckCircle } from "lucide-react";
+
+export default function PageIcon({ title, active }) {
+  const base = "h-4 w-4";
+  const color = active ? "text-[#F59D0E]" : "text-gray-500";
+  const props = { className: `${base} ${color}` };
+
+  switch (title) {
+    case "Info":
+      return <Info {...props} />;
+    case "Details":
+    case "Other":
+      return <FileText {...props} />;
+    case "Ending":
+      return <CheckCircle {...props} />;
+    default:
+      return <FileText {...props} />;
+  }
+}

0ea498edbdde9203d71b86342c917b47e1952c86
Kevin Yoon <brood915@gmail.com>
2025-06-14 22:57:20 -0400

    removed comments

--- src/components/AddPageButton.jsx
+++ src/components/AddPageButton.jsx
@@ -4,11 +4,9 @@ import ConnectorDashes from "./ConnectorDashes";
 export default function AddPageButton({ onAdd }) {
   return (
     <div className="flex items-center">
-      {/* Final dashed connector */}
       <div className="w-10 h-6 flex items-center">
         <ConnectorDashes />
       </div>
-      {/* Add page button */}
       <button
         onClick={onAdd}
         className="inline-flex items-center gap-2 px-6 h-10 rounded-lg border border-gray-300 bg-white text-gray-700 font-medium shadow hover:bg-gray-50 whitespace-nowrap focus:outline-none transition-colors"
